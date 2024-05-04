5983a7521b3a5c790c906911ea43cc91e10b33e4
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-05-04 07:32:37 +0300

    Fixed Navbar-Stays at top

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { SearchInput } from "./SearchInput";
+import React from 'react';
+import { SearchInput } from './SearchInput';
 
 export const Navbar = () => {
   return (
-    <div className="flex flex-row items-center p-3 bg-slate-950 text-slate-50">
+    <div className="flex flex-row items-center p-3 bg-slate-950 text-slate-50 fixed top-0 w-full z-10">
       <button type="submit" className="mx-3 text-gray-400">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -49,9 +49,9 @@ export const Navbar = () => {
         </button>
 
         <button className="mx-2 p-2 hover:bg-gray-800 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-          <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
-        </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+          </svg>
         </button>
       </div>
     </div>
