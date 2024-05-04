c084a7ac61c8ace9ea55287d5d2f889c6c016e62
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-05-04 04:40:26 +0300

    Added a sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import Nav from './Nav'
-import You from './You'
-import Subscriptions from './Subscriptions'
-import Explore from './Explore'
-import More from './More'
-import Footer from './Footer'
-import Settings from './Settings'
+import React, { useState } from 'react';
+import Nav from './Nav';
+import You from './You';
+import Subscriptions from './Subscriptions';
+import Explore from './Explore';
+import More from './More';
+import Footer from './Footer';
+import Settings from './Settings';
 
 const Sidebar = () => {
-  return (
-    <div className='w-[250px] mr-2 p-2 bg-black text-white'>
-        <Nav/>
-        <You/>
-        <Subscriptions/>   
-        <Explore/>
-        <More/>
-        <Settings/>
-        <Footer/>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Sidebar
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      {/* Hamburger icon */}
+      <button className="fixed top-4 left-4 z-50" onClick={toggleSidebar}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+          <path d="M3 12h18M3 6h18M3 18h18"></path>
+        </svg>
+      </button>
+      {/* Sidebar */}
+      <div className={`fixed top-[64px] left-0 bottom-0 bg-black text-white w-[250px] transition-all duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        <Nav />
+        <You />
+        <Subscriptions />
+        <Explore />
+        <More />
+        <Settings />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
