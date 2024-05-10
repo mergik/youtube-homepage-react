ba097ab9eb004e35a608ef4ce86a0b3452848ed1
Michael Mutinda <mmike8449@gmail.com>
2024-05-10 12:39:33 -0400

    incorporated video details from youtube in the video card display

--- src/hooks/useYoutubeSearch.js
+++ src/hooks/useYoutubeSearch.js
@@ -5,9 +5,23 @@ export const useYoutubeSearch = () => {
 
     const fetchSearchResults = async (searchQuery) => {
         try {
-            const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchQuery}&key=${import.meta.env.VITE_APIKEY}`);
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${searchQuery}&key=${import.meta.env.VITE_APIKEY}`);
             const data = await response.json();
-            setSearchResults(data.items || []);
+            
+            const videoIds = data.items.map(item => item.id.videoId).join(',');
+
+            const videoDetailsResponse = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoIds}&key=${import.meta.env.VITE_APIKEY}`);
+            const videoDetailsData = await videoDetailsResponse.json();
+
+            const searchResultsWithDetails = data.items.map((item, index) => {
+                return {
+                    id: item.id.videoId,
+                    snippet: item.snippet,
+                    statistics: videoDetailsData.items[index].statistics
+                };
+            });
+
+            setSearchResults(searchResultsWithDetails);
         } catch (error) {
             console.error('ERROR FETCHING DATA FROM YOUTUBE API!', error);
         }
