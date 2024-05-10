b948d9d170223ec6fe8350a87f0b44d5d5aadadb
mergik <122933591+mergik@users.noreply.github.com>
2024-05-10 21:08:39 +0300

    Built search results page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { Navbar } from './components/Navigation/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
-import { Videobody } from './components/Videobody/Videobody'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navigation/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Videobody } from './components/Videobody/Videobody';
+import { AboutUs } from './pages/AboutUs/AboutUs';
+import { ContactUs } from './pages/ContactUs/ContactUs';
+import SearchResults from './pages/SearchResults/SearchResults';
+
 
 function App() {
  
   return (
-    <div className='font-roboto'>
-      <Navbar/>
-      
-      <div className='flex min-h-screen'>
-        <Sidebar/>
-        <Videobody/>
-        
+    <Router>
+      <div className='font-roboto'>
+        <Navbar />
+
+        <div className='flex min-h-screen'>
+          <Sidebar />
+          {/* Routes replace the Switch component */}
+          <Routes>
+            <Route path="/" element={<Videobody />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+            
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
