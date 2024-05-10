a9bfa187fa6658d7b36f85341cf14ebe960191de
mergik <122933591+mergik@users.noreply.github.com>
2024-05-10 22:54:41 +0300

    Implemented search bar and navigation
    - Fixed videocard useEffect

--- src/components/Navigation/SearchInput.jsx
+++ src/components/Navigation/SearchInput.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useYoutubeSearch } from "../../hooks/useYoutubeSearch";
+import { useNavigate } from "react-router-dom";
+
 
 export const SearchInput = () => {
+  const { searchResults, fetchSearchResults } = useYoutubeSearch();
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    await fetchSearchResults(searchTerm);
+    navigate(`/search?q=${searchTerm}`);
+    console.log(searchResults);
+  }
+
   return (
     <div className="flex mx-5 basis-1/2 justify-center">
       {/* Search Field */}
       <form className="flex items-center h-10 z-10 basis-[90%] max-w-[600px]">
         
         <label className="flex rounded-l-full px-5 w-full h-full border border-gray-500 focus-within:border focus-within:border-blue-500 focus-within:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-2">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-          </svg>
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full bg-black text-left focus:outline-none"
-        />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full bg-black text-left focus:outline-none"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+          />
         </label>
         
         <button
           type="submit"
           className="rounded-r-full w-16 h-10 px-3 text-center justify-center border border-l-0 border-gray-500 bg-gray-800 hover:bg-gray-700"
+          onClick={handleSearch}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
