9d3d97a4ac59fe80eaaa4ae11007de51e4a49419
KennedyRyan-code <kenryan49@gmail.com>
2024-05-19 10:11:52 +0300

    restored the background color to black

--- src/components/Sidebar/Subscriptions.jsx
+++ src/components/Sidebar/Subscriptions.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Subscriptions = () => {
 
   return (
-    <div className="border-b space-y-1 text-sm border-gray-700 bg-green-500">
+    <div className="border-b space-y-1 text-sm border-gray-700">
       <p className="rounded-lg p-2 text-lg items-center hover:bg-gray-800 cursor-pointer space-x-5">
         Subscriptions
       </p>
