ed17f2089aef451dc1c3c07f1a79a3bd59b7d665
KennedyRyan-code <kenryan49@gmail.com>
2024-05-20 12:10:43 +0300

    Implemented dynamic logic for Channels from searchResults & added a Show more button

--- src/components/Sidebar/Subscriptions.jsx
+++ src/components/Sidebar/Subscriptions.jsx
@@ -1,71 +1,37 @@
 import React from "react";
+import { useState, useContext } from "react";
+import { ResultsContext } from "../../context/ResultsContext";
+
 
 
 const Subscriptions = () => {
+const { searchResults } = useContext(ResultsContext);
+const [ displayedResults, setDisplayedResults ] = useState(5);
+
+const handleShowMore = () => {
+  setDisplayedResults(prevDisplayedResults => prevDisplayedResults + 5);
+};
+
 
   return (
     <div className="border-b space-y-1 text-sm border-gray-700">
       <p className="rounded-lg p-2 text-lg items-center hover:bg-gray-800 cursor-pointer space-x-5">
         Subscriptions
       </p>
-     <section className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
-        <img
-          src="https://picsum.photos/16"
-          className=" rounded-full w-[25px] h-[25px] "
-          alt="channel_icon"
-        />
-        <div className="justify-items-center pl-4">
-          <p className="text-wrap w-[272px] text-gray-50">Citizen TV Kenya</p>
-        </div>
-      </section>
-
-      <section className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
-        <img
-          src="https://picsum.photos/16"
-          className=" rounded-full w-[25px] h-[25px] "
-          alt="channel_icon"
-        />
-        <div className="justify-items-center pl-4">
-          <p className="text-wrap w-[272px] text-gray-50">React Native</p>
-        </div>
-      </section>
-
-      <section className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
-        <img
-          src="https://picsum.photos/16"
-          className=" rounded-full w-[25px] h-[25px] "
-          alt="channel_icon"
-        />
-        <div className="justify-items-center pl-4">
-          <p className="text-wrap w-[272px] text-gray-50">JavaScript Mastery</p>
-        </div>
-      </section>
-
-      <section className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
-        <img
-          src="https://picsum.photos/16"
-          className=" rounded-full w-[25px] h-[25px] "
-          alt="channel_icon"
-        />
-        <div className="justify-items-center pl-4">
-          <p className="text-wrap w-[272px] text-gray-50">Payload</p>
-        </div>
-
-      </section>
-
-      <section className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
-        <img
-          src="https://picsum.photos/16"
-          className=" rounded-full w-[25px] h-[25px] "
-          alt="channel_icon"
-        />
-        <div className="justify-items-center pl-4">
-          <p className="text-wrap w-[272px] text-gray-50">Safari Compus</p>
-        </div>
-
-      </section>
-
+      {searchResults?.slice(0, displayedResults).map((data) => (
+        <section key={data.id} className="flex my-4 space-x-2 text-lg items-center pl-3 pb-2 cursor-pointer hover:bg-gray-600">
+            <img
+              src={data.snippet.thumbnails.default.url}
+              className=" rounded-full w-[25px] h-[25px] "
+              alt="channel_icon"
+            />
+            <div className="justify-items-center pl-4">
+              <p className="text-wrap w-[272px] text-gray-50">{data.snippet.channelTitle}</p>
+            </div>
+          </section>
+      ))}
       {/* Show More */}
+      { searchResults?.length > displayedResults && ( // Conditional rendering for "Show More" button
       <div className="flex rounded-lg p-2 items-center text-lg pl-4 hover:bg-gray-800 cursor-pointer space-x-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -81,8 +47,9 @@ const Subscriptions = () => {
             d="m8.25 4.5 7.5 7.5-7.5 7.5"
           />
         </svg>
-        <p className="pl-4">Show More</p>
+        <p className="pl-4" onClick={handleShowMore}>Show More</p>
       </div>
+      )}
 
     </div>
   );
