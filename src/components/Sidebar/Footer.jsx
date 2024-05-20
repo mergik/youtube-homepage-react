67c72855456940dc460008bab9fc37ffaa2e8464
mergik <122933591+mergik@users.noreply.github.com>
2024-05-20 17:48:45 +0300

    Rearranged links & removed empty routes

--- src/components/Sidebar/Footer.jsx
+++ src/components/Sidebar/Footer.jsx
@@ -3,22 +3,22 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="p-2 text-sm text-left text-gray-300">
-      <div className="flex flex-wrap m-2 space-x-2">
-        <a href="#">About</a>
-        <a href="#">Press</a>
-        <a href="#">Copyright</a>
-        <a href="#">Contact Us</a>
-        <a href="#">Creators</a>
-        <a href="#">Advertise</a>
-        <a href="#">Developers</a>
+      <div className="flex flex-wrap m-2 gap-x-2">
+        <Link to="/about" className="hover:text-white">About</Link>
+        <Link to="/contact" className="hover:text-white">Contact Us</Link>
+        <Link to="/copyright" className="hover:text-white">Copyright</Link>
       </div>
 
-      <div className="flex flex-wrap m-2 space-x-2">
-        <a href="#">Terms</a>
-        <a href="#">Privacy</a>
-        <a href="#">Policy & Safety</a>
-        <a href="#">How YouTube works</a>
-        <a href="#">Test New Features</a>
+      <div className="flex flex-wrap m-2 gap-x-2">
+        <a href="#" className="hover:cursor-default">Press</a>
+        <a href="#" className="hover:cursor-default">Creators</a>
+        <a href="#" className="hover:cursor-default">Advertise</a>
+        <a href="#" className="hover:cursor-default">Developers</a>
+        <a href="#" className="hover:cursor-default">Terms</a>
+        <a href="#" className="hover:cursor-default">Privacy</a>
+        <a href="#" className="hover:cursor-default">Policy & Safety</a>
+        <a href="#" className="hover:cursor-default">How YouTube works</a>
+        <a href="#" className="hover:cursor-default">Test New Features</a>
       </div>
 
       <p className="m-2 font-light text-xs text-gray-500">© 2024 - 20955 Task Force</p>
