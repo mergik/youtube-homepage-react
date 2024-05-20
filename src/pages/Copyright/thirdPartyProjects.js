579fbfa0ca1529b7d44c8ddcc6be613dc2959208
mergik <122933591+mergik@users.noreply.github.com>
2024-05-20 20:26:30 +0300

    Added the special thanks page

---
+++ src/pages/Copyright/thirdPartyProjects.js
@@ -0,0 +1,20 @@
+export const THIRDPARTYPROJECTS = [
+    {
+        key: 1,
+        projecttitle: "Hero Icons",
+        description: "Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.",
+        weblink: "https://heroicons.com/",
+    },
+    {
+        key: 2,
+        projecttitle: "Hero Patterns",
+        description: "A collection of repeatable SVG background patterns for you to use on your web projects.",
+        weblink: "https://heropatterns.com/",
+    },
+    {
+        key: 3,
+        projecttitle: "Icons8",
+        description: "The ultimate design kit for creatives and developers",
+        weblink: "https://icons8.com",
+    },
+];
